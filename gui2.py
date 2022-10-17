cb7cd652ab7b3c87b093bac23c7d6e17ea3c212c
nicolas <nicolasrubino15@gmail.com>
2022-10-17 12:04:31 -0500

    write exterieur pdf

--- gui2.py
+++ gui2.py
@@ -18,21 +18,25 @@ root.configure(background="#4a4a4a")
 
 def Open_Audio_File():
     root.filename = filedialog.askopenfilename(initialdir="",title="Veuillez choisir votre fichier audio")
-    pdf_text = aws_handler.upload_and_transcribe(root.filename,os.path.basename(root.filename))
-    convert_file(os.path.basename(root.filename),pdf_text)
-    aws_handler.clean()
+    # pdf_text = aws_handler.upload_and_transcribe(root.filename,os.path.basename(root.filename))
+    convert_file(os.path.basename(root.filename),"un homme. Je ne suis pas tout à fait satisfait avec le, donc j'ai décidé de changer de librairie pour celle de. Le menu aurait un bouton pour sélectionner un pré enregistrés, puis ensuite courir la reconnaissance vocale et finalement pour nous donner, avec le résultat en important, la librairie Tes quinté Il faut ajouter une grandeur de fenêtres avec la fonction de géométrie, une couleur de bakoung avec configure et une loupe dans laquelle les éléments de la route de roulera reste. Comme le reste, comme le texte, des images est sont ajoutés avec la fonction de Pâques, JQ doit maintenant adapter avec mon projet. Une des premières choses que je dois ajouter est un bouton pour ouvrir. Le fallait explorer.")
+    # aws_handler.clean()
 
 def convert_file(filename, text):
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font("Arial",size=10)
     pdf.cell(200, 10, txt=filename, ln=1, align='C')
+    pdf.set_auto_page_break(auto=True, margin=15)
     line = 4
-    for i in text.split("."):
-        pdf.cell(200, 10, txt=i+".", ln=line, align='L')
-        line = line+1
+    pdf.multi_cell(0,5,txt=text)
+    # for i in text.split("."):
+    #     if len(i) > 115:
+    #         for j in i.split(" "):
+    #         pdf.cell(200, 10, txt=j+" " , ln=line, align='L')
+    #     pdf.cell(200, 10, txt=i+"."+ str(len(i)) , ln=line, align='L')
+    #     line = line+1
     pdf.output("created.pdf")
-
 photo = PhotoImage(file="mic.png")
 icon = Label(image=photo, background="#4a4a4a")
 icon.pack(padx=5, pady=5)
